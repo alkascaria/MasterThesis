21cbe4e0e7bbf23dca5d2dc7cf7eeb7923fb2388
alka scaria <alka.scaria@tukl.onmicrosoft.com>
2023-05-15 19:22:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,46 @@ import { Editor } from '@tinymce/tinymce-react';
 
 export default function App() {
   const editorRef = useRef(null);
+
   const log = () => {
     if (editorRef.current) {
       console.log(editorRef.current.getContent());
     }
   };
+
+  //To download the source code as HTML or txt filenp
+  const saveContentAsHTML = () => {
+    if (editorRef.current) {
+      let content = editorRef.current.getContent();
+      let blob = new Blob([content], { type: 'text/html' });
+      let link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'content.html';
+      link.download = 'content.txt';
+      link.click();
+    }
+  };
+
+  const saveContentToDatabase = () => {
+    if (editorRef.current) {
+      let content = editorRef.current.getContent();
+      
+      fetch('http://localhost:3000/api/saveContent', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ content: content })
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    }
+  };
+  
+
   return (
     <>
       <Editor
@@ -25,11 +60,13 @@ export default function App() {
           toolbar: 'undo redo | blocks | ' +
             'bold italic forecolor | alignleft aligncenter ' +
             'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
+            'removeformat | help | code',
           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
         }}
       />
       <button onClick={log}>Log editor content</button>
+      <button onClick={saveContentAsHTML}>Save as HTML</button>
+      <button onClick={saveContentToDatabase}>Save to DB</button>
     </>
   );
 }
