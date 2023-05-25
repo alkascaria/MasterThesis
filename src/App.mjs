e1ff5e4191ea049ba7a8b4ea3b588b3a3f0ccfb5
alka scaria <alka.scaria@tukl.onmicrosoft.com>
2023-05-25 15:30:24 +0200

    drop down menu for symbols

--- src/App.mjs
+++ src/App.mjs
@@ -1,16 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import axios from 'axios';
 
 export default function App() {
   const editorRef = useRef(null);
-  const [fetchedImages, setFetchedImages] = useState([]);
 
-  // Creating a cutom plugin to fetch images from the database
+  // Creating a cutom plugin to fetch images from the database and use a drop down menu
   useEffect(() => {
 
     // since we are trying to add a custom plugin in the useEffect hook, it is important to ensure that tinymce is fully loaded 
-    // before attempting to call window.tinymce.PluginManager.add
+    // before attempting to call 'window.tinymce.PluginManager.add'
     // This code sets up an interval that checks every 100ms if window.tinymce is defined, and if it is, it clears the interval 
     // and then adds the custom plugin. If the component unmounts, it also clears the interval to prevent memory leaks. 
        
@@ -18,27 +17,48 @@ export default function App() {
       if (window.tinymce) {
         clearInterval(timer);
         window.tinymce.PluginManager.add('customPlugin', function(editor) {
-          editor.ui.registry.addButton('customPlugin', {
-            text: 'Fetch Images',
-            onAction: async function() {
-              try {
-                const response = await axios.get('http://127.0.0.1:5000/api/images');
-                console.log(response.data);
-                alert('Images fetched from the database!');
-                setFetchedImages(response.data);
-                const images = response.data;
-                // Insert the first image from the fetched images into the editor's content
-                if (images.length > 0) {
-                  const firstImage = images[0];
-                  //The DB formant is (id,acronym,description,symbol), the below 2 line changes according to the format of the DB
-                  const imageUrl = firstImage.symbol; 
-                  const imageAlt = firstImage.acronym; 
-                  editor.insertContent(`<img src="${imageUrl}" alt="${imageAlt}">`);
+          let images = [];
+        
+          const fetchImages = async () => {
+            try {
+              const response = await axios.get('http://127.0.0.1:5000/api/images'); //Fetching images from the database
+              images = response.data;
+        
+              // Calling `editor.ui.registry.updateMenuItem` function to dynamically update the menu items with the fetched images
+
+              // The DB formant is (id,acronym,description,symbol), the below 2 line changes according to the format of the DB
+
+              editor.ui.registry.updateMenuItem('imageMenuButton', {
+                fetch: function(callback) {
+                  const items = images.map((image, index) => ({
+                    type: 'menuitem',
+                    text: image.symbol, 
+                    onAction: function() {
+                      editor.insertContent(`<img src="${image.symbol}" alt="${image.acronym}">`);
+                    }
+                  }));
+                  callback(items);
                 }
-              } catch (error) {
-                console.error(error);
-                alert('Error fetching images from the database.');
-              }
+              });
+            } catch (error) {
+              console.error(error);
+            }
+          };
+          // Fetch the images when the plugin is initialized
+          fetchImages();
+          
+          // Register the menu button - Custom dropdown menu
+          editor.ui.registry.addMenuButton('imageMenuButton', {
+            text: 'Insert Symbol',
+            fetch: function(callback) {
+              const items = images.map((image, index) => ({
+                type: 'menuitem',
+                text: image.acronym, 
+                onAction: function() {
+                  editor.insertContent(`<img src="${image.symbol}" alt="${image.acronym}">`);
+                }
+              }));
+              callback(items);
             }
           });
         });
@@ -115,19 +135,13 @@ export default function App() {
             'undo redo | blocks | image ' +
             'bold italic forecolor | alignleft aligncenter ' +
             'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help | code | customPlugin',
+            'removeformat | help | code | imageMenuButton',
           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
         }}
       />
       <button onClick={log}>Log editor content</button>
       <button onClick={saveAsHTML}>Save as HTML</button>
       <button onClick={saveToDB}>Save to DB</button>
-
-      {/* Displaying fetched images */}
-      <h2>Fetched Images</h2>
-      {fetchedImages.map(image => (
-        <img key={image.id} src={image.symbol} alt={image.acronym} style={{ width: '100px' }} />
-      ))}
     </>
   );
 }
